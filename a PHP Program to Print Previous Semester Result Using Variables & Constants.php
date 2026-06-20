ea73f33b26275d2d840efbbc987fcba886865007
SOMU <maitysomumaity@gmail.com>
2026-06-20 08:39:22 +0530

    Add files via upload

---
+++ a PHP Program to Print Previous Semester Result Using Variables & Constants.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Somu";
+$semester = 4;
+$percentage = 78.50;
+
+define("COLLEGE", "ABC College");
+
+echo "College: ".COLLEGE."<br>";
+echo "Student Name: ".$name."<br>";
+echo "Semester: ".$semester."<br>";
+echo "Percentage: ".$percentage."%";
+?>
