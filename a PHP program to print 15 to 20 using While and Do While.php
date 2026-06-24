4b5b056f4e3331aa52625f18c81c4b2c350c4e70
SOMU <maitysomumaity@gmail.com>
2026-06-24 18:58:15 +0530

    Add files via upload

---
+++ a PHP program to print 15 to 20 using While and Do While.php
@@ -0,0 +1,17 @@
+<?php
+echo "Using While Loop:<br>";
+
+$i = x;
+while ($i <= y) {
+    echo $i . " ";
+    $i++;
+}
+
+echo "<br><br>Using Do While Loop:<br>";
+
+$j = x;
+do {
+    echo $j . " ";
+    $j++;
+} while ($j <= y);
+?>
